d78354300e0ff740c5e0cd606dda768de9e9e0fb
GeonwooVincentKim <kdsnop@gmail.com>
2021-03-04 14:42:04 +0900

    [Update] Delete unnecessary codes

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import sys
 import time
 
-# sys.path.insert(0, "/Attribute/body.py")
-# import Body
 from Attribute.body import *
-# from Attribute.body import Body
+
 
 def main():
     print("Hello world!!")
