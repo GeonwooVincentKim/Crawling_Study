064baf2b4a89f409524eb5e5e6a1d6889218e059
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-01 11:01:54 +0900

    [Feature] - Added config_info.py file

--- News/Bigkinds/bigkinds_crawler.py
+++ News/Bigkinds/bigkinds_crawler.py
@@ -1,5 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import mysql.connector as connector
- 
 
+import sys, os.path
+config_dir = (
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__), '..'
+        )
+    )
++ '/collector/')
+sys.path.append(config_dir)
+import config_info
+
+print(config_info.PARAMS)
